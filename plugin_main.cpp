4c4f72725189f591ab42c3b1998c8a98cca935fe
oscarluiis <tinocoscar11@unitec.edu>
2021-02-10 19:55:48 -0600

    Mas funciones

--- plugin_main.cpp
+++ plugin_main.cpp
@@ -79,6 +79,26 @@ extern "C" ErrorCode handleSyscall(uint32_t *regs, void *mem, MemoryMap *mem_map
             rlutil::resetColor();
             return ErrorCode::Ok;
         }
+        case 24:
+        {   
+            //limpiar la pantalla
+            rlutil::cls();
+            return ErrorCode::Ok;
+        }
+        
+        case 25:
+        {
+            std::cout<<"No se deberia ver el cursor"<<'\n';
+            rlutil::hidecursor();
+            return ErrorCode::Ok;
+        }
+        
+        case 26:
+        {
+            std::cout<<"Se deberia ver el cursor"<<'\n';
+            rlutil::showcursor();
+            return ErrorCode::Ok;
+        }
         default:
             if (v0 > 20 && v0 <= 50)
             {
